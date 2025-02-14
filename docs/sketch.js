4d0f596406a499a59608214c31216c6a32e33ae3
Nico <nico.esg.rey@gmail.com>
2025-02-14 21:08:38 +0000

    Iddle/Flying and Death  animation added

--- docs/sketch.js
+++ docs/sketch.js
@@ -7,6 +7,7 @@ let page = null;
 let shop = null;
 let inventory = null;
 let playerImg = null;
+let spriteSheet = null;
 
 function setup() {
 
@@ -29,4 +30,7 @@ function draw() {
 
 function preload() {
     playerImg = loadImage('assets/player1.png');
+    //playerFloor = loadImage('assets/playerFloor.png');
+    spriteSheet = loadImage('assets/playerFloor.png'); // Load your sprite sheet
+    deathSpriteSheet = loadImage('assets/playerDeath.png'); // Death animation sprite sheet
 }
